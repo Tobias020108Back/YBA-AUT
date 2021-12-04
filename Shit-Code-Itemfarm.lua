afff0a7776d51a3858d9254bcc42f6a7c9292194
Tobias020108 <84468331+Tobias020108Back@users.noreply.github.com>
2021-12-04 04:33:07 +0200

    Update Shit-Code-Itemfarm.lua

--- Shit-Code-Itemfarm.lua
+++ Shit-Code-Itemfarm.lua
@@ -1,13 +1,13 @@
 repeat 
 if _G.FakeInvis == true then
-    wait(2)
+    wait(4)
     _G.farm = true
 end
     until _G.FakeInvis == true
 if _G.FakeInvis == false then
     wait(1)
     _G.farm = false
-    end
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Tobias020108Back/YBA-AUT/main/World-Of-Stands-Itemfarm.lua"))()
-wait()
+end
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Tobias020108Back/YBA-AUT/main/FakeInvis.lua"))()
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Tobias020108Back/YBA-AUT/main/World-Of-Stands-Itemfarm.lua"))()
