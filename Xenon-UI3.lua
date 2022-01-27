e4baa788227e634002d8f6b4bbc3a6b081a6d069
Tobias020108 <84468331+Tobias020108Back@users.noreply.github.com>
2022-01-28 01:25:22 +0200

    Update Xenon-UI3.lua

--- Xenon-UI3.lua
+++ Xenon-UI3.lua
@@ -140,7 +140,7 @@ function LibraryV2.UI(Name)
 
     local Library
     Library = {
-        _UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Tobias020108Back/YBA-AUT/main/Xenon-UI2")),
+        _UI = game:HttpGet("https://raw.githubusercontent.com/Tobias020108Back/YBA-AUT/main/Xenon-UI2"),
         Name = Name or "Untitled",
         Tabs = {},
         State = false,
