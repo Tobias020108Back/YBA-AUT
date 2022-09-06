3d35e32ed4008f000693025248de2b2ca9ab492e
Tobias020108 <84468331+Tobias020108Back@users.noreply.github.com>
2022-09-06 06:54:56 +0200

    Create XTrey10x-Hub.lua

---
+++ XTrey10x-Hub.lua
@@ -0,0 +1,29 @@
+--Not my code
+
+if game.PlaceId == 2274701129 or game.PlaceId == 7034703884 or game.PlaceId == 2124841545 or game.PlaceId == 9612879759 or game.PlaceId == 7034704024 or game.PlaceId == 4586100405 or game.PlaceId == 4147393260 or game.PlaceId == 8192000095 or game.PlaceId == 9621187271 or game.PlaceId == 4147411085  or game.PlaceId == 4586100405 or game.PlaceId == 2274711816 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xtrey10x/xtrey10x-hub/main/xtrey10xhubdragonballmultiverse"))()
+end
+if game.PlaceId == 10266164381 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xtrey10x/xtrey10x-hub/main/xtrey10xhubloodlines"))()
+end
+if game.PlaceId == 3978370137 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xtrey10x/xtrey10x-hub/main/Xtrey10xGpo"))()
+end
+if game.PlaceId == 6152116144 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xtrey10x/xtrey10x-hub/main/xtrey10x-Projectslaers13453fg.lua"))()
+end
+if game.PlaceId == 863266079 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xtrey10x/xtrey10x-hub/main/Xtrey10x%20Hub%20-%20APR2.lua"))()
+end
+if game.PlaceId == 4616652839 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xtrey10x/xtrey10x-hub/main/Xtrey10x%20Hub%20Shindo.lua"))()
+end
+if game.PlaceId == 8204899140 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xtrey10x/xtrey10x-hub/main/football%20fusion.lua"))()
+end
+if game.PlaceId == 286090429 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xtrey10x/xtrey10x-hub/main/Xtrey10x%20-%20Arsenal.lua"))()
+end
+if game.PlaceId == 7540727946 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xtrey10x/xtrey10x-hub/main/Hoops%20Life.lua"))()
+end
