ca776c572b0dde0229f4be38ed87df693500395b
Tobias020108 <84468331+Tobias020108Back@users.noreply.github.com>
2023-08-01 00:08:20 +0200

    Update AFS-Dataloss-GUI.lua

--- AFS-Dataloss-GUI.lua
+++ AFS-Dataloss-GUI.lua
@@ -1,84 +1,87 @@
 local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
 
 local X = Material.Load({
-	Title = "AFS - tobias020108back.",
-	Style = 3,
-	SizeX = 500,
-	SizeY = 350,
-	Theme = "Dark",
-	ColorOverrides = {
-		MainFrame = Color3.fromRGB(35,35,35)
-	}
+    Title = "AFS - tobias020108back.",
+    Style = 3,
+    SizeX = 500,
+    SizeY = 350,
+    Theme = "Dark",
+    ColorOverrides = {
+        MainFrame = Color3.fromRGB(35,35,35)
+    }
 })
- 
+
 local Y = X.New({
-	Title = "Main"
+    Title = "Main"
 })
 
 local A = Y.Button({
-	Text = "Selected no Type",
-	Callback = function()
-		if Type == "Start Dataloss" then
-for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.MainGui.Pets.Main.Scroll:GetDescendants()) do
-if v.Name == "UID" then
-local args = {
-    [1] = {
-        [string.rep("B", 4200000)] = v.Value
-    },
-    [2] = "AFS_Is_Dogshit",
-    [3] = 3
-}
+    Text = "You have not selected any Dataloss Type",
+    Callback = function()
+        if Type == "Start Dataloss" then
+            for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.MainGui.Pets.Main.Scroll:GetDescendants()) do
+                if v.Name == "UID" then
+                    local args = {
+                        [1] = {
+                            [string.rep("B", 4200000)] = v.Value
+                        },
+                        [2] = "AFS_Is_Dogshit",
+                        [3] = 3
+                    }
 
-game:GetService("ReplicatedStorage").Remote.SaveTeam:FireServer(unpack(args))
-break
-end
-end
-TextField:SetText("Started")
-		elseif Type == "Undo Dataloss" then
-for i = 1,100 do
-local args = {
-    [1] = i
-}
+                    game:GetService("ReplicatedStorage").Remote.SaveTeam:FireServer(unpack(args))
+                    break
+                end
+            end
+            TextField:SetText("Started")
+        elseif Type == "Undo Dataloss" then
+            for i,v in pairs(game.Players.LocalPlayer.PlayerGui.MainGui.Pets.TeamsList.Main.Scroll:GetDescendants()) do
+                if v.Name == "TeamName" and v.Text == "AFS_Is_Dogshit" then
+                    local args = {
+                        [1] = v.Parent.LayoutOrder
+                    }
 
-game:GetService("ReplicatedStorage").Remote.DeleteTeam:FireServer(unpack(args))
-end
-TextField:SetText("Undone")
-		end
-	end
+                    game:GetService("ReplicatedStorage").Remote.DeleteTeam:FireServer(unpack(args))
+                    TextField:SetText("Undone")
+                    break
+                end
+            end
+        end
+    end
 })
 
 local D = Y.Dropdown({
-	Text = "Type",
-	Callback = function(Value)
-		getgenv().Type = Value
-	end,
-	Options = {
-		"Start Dataloss",
-		"Undo Dataloss"
-	}
+    Text = "Dataloss Type",
+    Callback = function(Value)
+        getgenv().Type = Value
+    end,
+    Options = {
+        "Start Dataloss",
+        "Undo Dataloss"
+    }
 })
 
 local AE = Y.Button({
-	Text = "https://discord.gg/EvTCW7CXSK",
-	Callback = function()
-		print("https://discord.gg/EvTCW7CXSK")
-	end
+    Text = "https://discord.gg/EvTCW7CXSK",
+    Callback = function()
+        print("https://discord.gg/EvTCW7CXSK")
+    end
 })
 
 local AE2 = Y.Button({
-	Text = "Copy Discord Invite",
-	Callback = function()
-		setclipboard("https://discord.gg/EvTCW7CXSK")
-	end
+    Text = "Copy Discord Invite",
+    Callback = function()
+        setclipboard("https://discord.gg/EvTCW7CXSK")
+    end
 })
 
 getgenv().TextField = Y.TextField({
-  Text = "Status",
-  Type = "Default"
+    Text = "Status",
+    Type = "Default"
 })
 
 while task.wait() do
-if Type then
-A:SetText(Type)
-end
+    if Type then
+        A:SetText(Type)
+    end
 end
